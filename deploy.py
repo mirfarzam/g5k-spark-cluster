8064dbc15736fe2a89108006807a2eb653c6fed7
Seyedfarzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-02-09 20:35:07 +0100

    np

--- deploy.py
+++ deploy.py
@@ -50,7 +50,7 @@ masterNode = sh.ShellHandler(masterNode, "root")
 masterNode.execute('export JAVA_HOME="/root/java";export PATH=$JAVA_HOME/bin:$PATH;java -version;')
 ## get master IP
 shin, shout, sherr = masterNode.execute("ip route get 1.2.3.4 | awk '{print $7}'")
-masterIP = (shout[0])[0:-2]
+masterIP = (shout[0])
 masterAddress = "spark://{}:7077".format(str(masterIP))
 # ## Running Mater
 masterNode.execute("./spark/bin/spark-class org.apache.spark.deploy.master.Master &")
