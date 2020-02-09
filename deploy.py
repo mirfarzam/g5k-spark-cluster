8beaa1ad945d5d57c6562c2906a4aa19749a83ca
Seyedfarzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-02-09 16:31:51 +0100

    np

--- deploy.py
+++ deploy.py
@@ -45,13 +45,13 @@ shin, shout, sherr = masterNode.execute("ip route get 1.2.3.4 | awk '{print $7}'
 masterIP = (shout[0])[0:-2]
 masterAddress = "spark://{}:7077".format(str(masterIP))
 # ## Running Mater
-masterNode.execute("./spark/bin/spark-class org.apache.spark.deploy.master.Master")
+masterNode.execute("./spark/bin/spark-class org.apache.spark.deploy.master.Master &")
 
 for node in clusterNodes:
     print("running on worker : {}".format(node))
     worker = sh.ShellHandler(masterNode, "root")
     worker.execute('export JAVA_HOME="/root/java";export PATH=$JAVA_HOME/bin:$PATH;java -version;')
-    masterNode.execute("./bin/spark-class org.apache.spark.deploy.worker.Worker {}".format(masterAddress))
+    masterNode.execute("./bin/spark-class org.apache.spark.deploy.worker.Worker {} &".format(masterAddress))
     print("success on {}".format(node))
     
 
